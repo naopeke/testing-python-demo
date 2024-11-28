9371385e8778fa84e71cfc4829406c4f5108aa7c
naopeke <naopeke@protonmail.com>
2024-11-28 10:58:17 +0100

    loop

--- 01-variables/code.py
+++ 01-variables/code.py
@@ -140,3 +140,41 @@ if user_input in ("y", "Y"): #whether the input is "y" or "Y"
         print("You were off by one.")
     else: 
         print("Sorry, it's wrong")
+
+# loop
+number = 7
+while True:
+    user_input = input("Would you like to play? (y/n) ").lower()
+    if user_input == "n":
+        break
+
+    user_number = int(input("Guess out number: "))
+    if user_number == number:
+        print("You guessed correctly!")
+    elif abs(number - user_number) in (1, -1):
+    # elif number - user_number in (1, -1):
+        print("You were off by one.")
+    else: 
+        print("Sorry, it's wrong")
+    
+
+friends = ["Rolf", "Jen", "Bob", "Anne"]
+for friend in friends:
+    print(f"{friend} is my friend.")
+
+
+grades = [35, 67, 98, 200, 100]
+total = 0
+amount = len(grades)
+
+for grade in grades:
+    total += grade
+
+print(total / amount)
+
+
+grades = [35, 67, 98, 200, 100]
+total = sum(grades)
+amount = len(grades)
+
+print(total / amount)
