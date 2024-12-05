ef4931ff15dc91ed3bb6ad81d235ff7fb6039f10
naopeke <naopeke@protonmail.com>
2024-12-05 21:09:14 +0100

    destructuring variables

--- 01-variables/code.py
+++ 01-variables/code.py
@@ -272,3 +272,27 @@ for t in student_attendance.items():
 # ('Rolf', 96)
 # ('Bob', 80)
 # ('Anne', 100)
+
+for student, attendance in student_attendance.items():
+    print(t)
+# ('Rolf', 96)
+# ('Bob', 80)
+# ('Anne', 100)
+
+people =[("Bob", 42, "Mechanic"), ("James", 24, "Artist"), ("Harry", 32, "Lecturer")]
+
+for name, age, profession in people:
+    print(f"{"Name: {name}, Age: {age}, Profession: {profession}"}")
+
+for person in people:
+    print(f"Name: {person[0]}, Age: {person[1]}, Profession: {person[2]}")
+
+
+person = ("Bob", 42, "Mechanic")
+name, _, profession = person
+print(name, profession) # ageは不要のときアンダースコア Bob Mechanic
+
+head, *tail = [1, 2, 3, 4, 5]
+print(head)
+print(*tail)
+
