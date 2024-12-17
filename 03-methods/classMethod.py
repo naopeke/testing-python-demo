d99294a423a14b37def7c38f625a0c60dc996282
naopeke <naopeke@protonmail.com>
2024-12-17 10:29:11 +0100

    class method

---
+++ 03-methods/classMethod.py
@@ -0,0 +1,18 @@
+class ClassTest:
+    def instance_method(self):
+        print(f"called instance_method {self}")
+
+test = ClassTest()
+test.instance_method() # called instance_method <__main__.ClassTest object at 0x000001E2087C6A50>
+ClassTest.instance_method(test) # called instance_method <__main__.ClassTest object at 0x000001E2087C6A50>
+
+
+class ClassTest2:
+    def instance_method(self):
+        print(f"called instance_method {self}")
+    
+    @classmethod
+    def class_method(cls):
+        print(f"Called class_method of {cls}")
+
+ClassTest2.class_method() # Called class_method of <class '__main__.ClassTest2'>
