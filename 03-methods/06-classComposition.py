1b7dae7a09b89624604d6868262792a70b72aca2
naopeke <naopeke@protonmail.com>
2025-02-03 15:39:33 +0100

    class composition

---
+++ 03-methods/06-classComposition.py
@@ -0,0 +1,27 @@
+# class BookShelf:
+#     def __init__(self, quantity):
+#         self.quantity = quantity
+    
+#     def __str__(self):
+#         return f"BookShelf with {self.quantity} books."
+
+# shelf = BookShelf(300)
+# print(shelf) # BookShelf with 300 books.
+
+# class Book(BookShelf):
+#     def __init__(self, name, quantity):
+#         super().__init__(quantity)
+#         self.name = name
+
+#     def __str__(self):
+#         return f"Book {self.name}"
+
+# book = Book("Harry Potter", 120)
+# print(book) # BookShelf with 120 books.
+
+class BookShelf:
+    def __init__(self, *books):
+        self.books = books
+    
+    def __str__(self):
+        return f"BookShelf with {len(self.books)} books."
