ce7b2ffa8a379f9691347d573b0fc933be4fb94b
naopeke <naopeke@protonmail.com>
2025-02-05 12:57:16 +0100

    importing

---
+++ 03-methods/10-importing/10-importing2.py
@@ -0,0 +1,12 @@
+from importing import divide
+
+# import importing
+# importing.divide
+
+print(divide(10, 2)) # 5.0
+# mymodule.py: mymodule
+
+print(__name__) # __main__
+
+# import sys
+# print(sys.path)
---
+++ 03-methods/10-importing/importing.py
@@ -0,0 +1,6 @@
+def divide(dividend, divisor):
+    return dividend / divisor
+
+print("mymodule.py: ", __name__) # mymodule.py:  __main__
+
+import libs.mylib
