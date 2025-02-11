913c291789f08bf47e73954b9a07b97a1550c59d
naopeke <naopeke@protonmail.com>
2025-02-11 15:29:54 +0100

    14 first-class functions

--- 03-methods/14-firstClassFunctions.py
+++ 03-methods/14-firstClassFunctions.py
@@ -14,4 +14,30 @@ def calculate(*values, operator):
     return operator(*values)
 
 result = calculate(20, 4, operator=divide)
+print(result)
 
+
+# sequence は探索するリストやシーケンス。
+# expected は探したい値。
+# finder は、各要素を引数にとって何らかの操作をして、検索に使用する値を返す関数です。
+
+def search(sequence, expected, finder):
+    for elem in sequence:
+        if finder(elem) == expected:
+            return elem
+    raise RuntimeError(f"Could not find an element with {expected}")
+
+friends = [
+    {"name": "John Doe", "age":24},
+    {"name": "Jane Doe", "age":30},
+    {"name": "Richard Gere", "age":30},
+]
+
+def get_friend_name(friend):
+    return friend["name"]
+
+print(search(friends, "Richard Gere", get_friend_name))
+
+# lambda を使うことで、get_friend_name の代わりに一行で関数を作成することもできます。
+# lambda 引数: 式
+print(search(friends, "Richard Gere", lambda friend: friend["name"]))
