a40e4b8b09c8d3717c19e7a82abf1fb2cbed9767
naopeke <naopeke@protonmail.com>
2025-02-13 14:09:09 +0100

    17 decorating functions

--- 03-methods/17-decoratingFunctionsWithParameters.py
+++ 03-methods/17-decoratingFunctionsWithParameters.py
@@ -2,11 +2,23 @@ import functools
 
 user = {"username": "jose", "access_level": "guest"}
 
+# *arg（可変長位置引数）
+# *arg は 任意の数の位置引数 を受け取ることができる。
+# ここでは panel の値が *arg のタプル内に入る。
+
+# **kwargs（可変長キーワード引数）
+# **kwargs は 任意の数のキーワード引数 を受け取ることができる。
+# 例えば、もし get_admin_password が panel="admin", user="jose" のような呼び出しを許可していたら、user="jose" は kwargs に格納される。
+
+# *arg, **kwargs の役割
+# デコレーターはどんな関数にも適用できるようにするため、引数を固定せず *arg, **kwargs を使っている。
+# func(*arg, **kwargs) の形で元の関数 (get_admin_password) を呼び出すことで、デコレーターが関数のパラメータ構造を変更せずに処理を追加できる。
+
 def make_secure(func):
     @functools.wraps(func)
-    def secure_function(panel):
+    def secure_function(*arg, **kwargs):
         if user["access_level"] == "admin":
-            return func(panel)
+            return func(*arg, **kwargs)
         else:
             return f"No admin permissions for {user['username']}"
     return secure_function
