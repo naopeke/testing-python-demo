b57a55d7f5855ec97e630e26b966e1caae64a9a7
naopeke <naopeke@protonmail.com>
2025-02-13 14:05:12 +0100

    17 decorating function

---
+++ 03-methods/17-decoratingFunctionsWithParameters.py
@@ -0,0 +1,21 @@
+import functools
+
+user = {"username": "jose", "access_level": "guest"}
+
+def make_secure(func):
+    @functools.wraps(func)
+    def secure_function(panel):
+        if user["access_level"] == "admin":
+            return func(panel)
+        else:
+            return f"No admin permissions for {user['username']}"
+    return secure_function
+
+@make_secure
+def get_admin_password(panel):
+    if panel == "admin":
+        return "1234"
+    elif panel == "billing":
+        return "super_secure_password"
+
+print(get_admin_password("billing"))
