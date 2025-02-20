9202f36a48f68c200bd3720b48b642360e53d8a3
naopeke <naopeke@protonmail.com>
2025-02-20 11:44:03 +0100

    testing

--- 04-testing/post.py
+++ 04-testing/post.py
@@ -1,8 +1,17 @@
 class Post:
+# Post クラスのインスタンスを作成する際に呼び出されます。このメソッドは、title（タイトル）と content（コンテンツ）という2つの引数を受け取り、それぞれをインスタンス変数として設定します。
+# self.title = title と self.content = content によって、Post インスタンスが持つタイトルとコンテンツが保存されます。
     def __init__(self, title, content):
         self.title = title
         self.content = content
 
+# 現在、実装されていません。通常、__repr__ はインスタンスを文字列として表現する方法を定義するもので、主にデバッグや表示時に使用されます。
+# 実装されていないため、Post オブジェクトを出力してもデフォルトのオブジェクト表現が使われます。
+    def __repr__(self):
+        pass
+
+# Post オブジェクトを辞書形式で返します。返される辞書には、タイトル（title）とコンテンツ（content）がキーと値として含まれます。
+# このメソッドは、Post オブジェクトをJSONとして表現したい場合に役立ちます。例えば、WebアプリケーションのAPIレスポンスに使用されることが多いです。
     def json(self):
         return {
             'title': self.title,
--- 04-testing/tests/unit/test_post.py
+++ 04-testing/tests/unit/test_post.py
@@ -2,12 +2,16 @@ from unittest import TestCase
 from post import Post
 
 class PostTest(TestCase):
+# Post クラスを使って新しい投稿を作成し、そのインスタンスの title と content が期待した通りであることを確認します。
+# self.assertEqual('Test', p.title) と self.assertEqual('Test Content', p.content) によって、p の title と content が正しい値を持っているかをテストします。
     def test_create_post(self):
         p = Post('Test', 'Test Content')
 
         self.assertEqual('Test', p.title)
         self.assertEqual('Test Content', p.content)
-    
+
+#  Post クラスの json メソッドが期待した辞書を返すことを確認します。
+#  p.json() が {'title': 'Test', 'content': 'Test Content'} という辞書を返すことを self.assertDictEqual(expected, p.json()) で確認しています。   
     def test_json(self):
         p = Post('Test', 'Test Content')
         expected = {'title': 'Test', 'content': 'Test Content'}
