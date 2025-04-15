3dd082fb4df992b6524936ec519f80f967f2b562
naopeke <naopeke@protonmail.com>
2025-04-15 08:41:36 +0200

    system test

--- 05-flask/app/tests/system/base_test.py
+++ 05-flask/app/tests/system/base_test.py
@@ -3,3 +3,5 @@ from app import app
 
 class BaseTest(TestCase):
     def setUp(self):
+        app.testing = True
+        self.app = app.test_client
--- 05-flask/app/tests/system/test_home.py
+++ 05-flask/app/tests/system/test_home.py
@@ -1,11 +1,13 @@
-from unittest import TestCase
-from app import app
+from tests.system.base_test import BaseTest
 import json
 
 
 class TestHome(TestCase):
+    def setUp(self):
+        pass
+    
     def test_home(self):
-        with app.test_client() as c:
+        with self.app() as c: #self.appは base_test.pyの self.app = app.test_client
             resp = c.get('/')
 
             self.assertEqual(resp.status_code, 200)
